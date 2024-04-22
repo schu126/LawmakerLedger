1b5c1469aca31751686ec043d188c38f846d39ea
Samuel Chu <samueljchu0126@gmail.com>
2024-04-22 15:43:13 -0400

    app routing

--- server/app.py
+++ server/app.py
@@ -19,5 +19,24 @@ db.init_app(app)
 def root():
     return '<h1>LawmakerLedger</h1>'
 
+@app.route('/members', methods=['GET'])
+def all_members():
+    member_obj = Member.query.all()
+
+    member_dict=[]
+    for member in member_obj:
+        member_dict.append(member.to_dict())
+
+    return member_dict, 200
+
+@app.route('/members/<int:id>', methods=['GET'])
+def member_by_id():
+    member_id = Member.query.filter(Member.id == id).first()
+
+    if member_id is None:
+        return {"error": "Congressperson not found."}, 404
+    else:
+        return member_id.to_dict(), 200
+
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
